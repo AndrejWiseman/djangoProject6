d7595c3f09793a96de2341e0bbfdfb9a29ad22bf
Andrej <533737@gmail.com>
2024-05-08 12:55:44 +0200

    vercel db

--- djangoProject6/settings.py
+++ djangoProject6/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jk!#j_cfqqy70%wffxg293_))_r#vpd4+y-2ms0wy0qt5s=**k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', '.now.sh']
 
