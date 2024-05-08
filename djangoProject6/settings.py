de02bbe70713ef2f72611480545184f46c2e8cd3
Andrej <533737@gmail.com>
2024-05-08 13:40:57 +0200

    vercel db

--- djangoProject6/settings.py
+++ djangoProject6/settings.py
@@ -127,24 +127,24 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'mystaticfiles'),
-STATICFILES_DIRS = [
-    BASE_DIR / "mystaticfiles"
-],
-# STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
-STATICFILES_DIRS = [
-    BASE_DIR / "productionfiles"
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'mystaticfiles'),
+# STATICFILES_DIRS = [
+#     BASE_DIR / "mystaticfiles"
+# ],
+STATIC_ROOT = os.path.join(BASE_DIR, 'productionfiles')
+# STATICFILES_DIRS = [
+#     BASE_DIR / "productionfiles"
+# ]
 
-STORAGES = {
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-
-}
+# STORAGES = {
+#     "default": {
+#         "BACKEND": "django.core.files.storage.FileSystemStorage",
+#     },
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+#
+# }
 
 # MEDIA_URL = 'media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
