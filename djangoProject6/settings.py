e332123c3a5b78d00b3864eb9ba0e7eec602cd3c
Andrej <533737@gmail.com>
2024-05-08 15:42:37 +0200

    vercel db

--- djangoProject6/settings.py
+++ djangoProject6/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,8 +132,6 @@ STATIC_URL = '/static/'
 # ]
 STATICFILES_DIRS = [BASE_DIR/'static',]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
