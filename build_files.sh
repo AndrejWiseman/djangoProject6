0ff5754a4aad03a6f019122e44a8940666834940
Andrej <533737@gmail.com>
2024-05-08 14:48:16 +0200

    vercel db

--- build_files.sh
+++ build_files.sh
@@ -12,4 +12,5 @@ python -m pip install -r requirements.txt
 
 # Collect static files
 echo "Collecting static files..."
-python manage.py collectstatic --noinput --clear
+python manage.py collectstatic
+#python manage.py collectstatic --noinput --clear
